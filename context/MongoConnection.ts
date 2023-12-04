ac049aa78a74336f911cae0c5851a23f53ea041d
Pablo <220241@fppiramide.com>
2023-12-04 10:34:31 +0100

    first commit

---
+++ context/MongoConnection.ts
@@ -0,0 +1,29 @@
+import type { Collection, Db } from 'mongodb'
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const url = process.env.DB_CONN_STRING || 'mongodb://localhost:27017'
+const dbName = process.env.DB_NAME || 'noticiasDB'
+const collections: { [key: string]: Collection } = {}
+
+async function createMongoConnection (): Promise<any> {
+  try {
+    const client = await MongoClient.connect(url)
+    const db = client.db(dbName)
+    addCollections(db)
+    console.log('Connected to MongoDB')
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error)
+  }
+}
+
+const addCollections = (db: Db): any => {
+  collections.noticias = db.collection(
+    process.env.NOTICIAS_COLLECTION || 'noticias'
+  )
+}
+
+export default createMongoConnection
+export { collections }
